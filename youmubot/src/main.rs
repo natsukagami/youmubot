1508523ca9a59db1edf73fb79ac01db71734179c
Natsu Kagami <natsukagami@gmail.com>
2020-09-07 02:09:36 -0400

    Main: Is now running with `--no-default-features --features core`

--- youmubot/src/main.rs
+++ youmubot/src/main.rs
@@ -1,9 +1,10 @@
 use dotenv;
 use dotenv::var;
 use serenity::{
-    framework::standard::{DispatchError, StandardFramework},
+    client::bridge::gateway::GatewayIntents,
+    framework::standard::{macros::hook, CommandResult, DispatchError, StandardFramework},
     model::{
-        channel::{Channel, Message, Reaction},
+        channel::{Channel, Message},
         gateway,
         id::{ChannelId, GuildId, UserId},
         permissions::Permissions,
@@ -21,78 +22,80 @@ impl Handler {
     }
 }
 
+#[async_trait]
 impl EventHandler for Handler {
-    fn ready(&self, _: Context, ready: gateway::Ready) {
+    async fn ready(&self, _: Context, ready: gateway::Ready) {
         println!("{} is connected!", ready.user.name);
     }
 
-    fn message(&self, mut ctx: Context, message: Message) {
+    async fn message(&self, mut ctx: Context, message: Message) {
         self.hooks.iter().for_each(|f| f(&mut ctx, &message));
-    }
-
-    fn reaction_add(&self, ctx: Context, reaction: Reaction) {
-        ctx.data
-            .get_cloned::<ReactionWatcher>()
-            .send(reaction, true);
-    }
-
-    fn reaction_remove(&self, ctx: Context, reaction: Reaction) {
-        ctx.data
-            .get_cloned::<ReactionWatcher>()
-            .send(reaction, false);
     }
 }
 
 /// Returns whether the user has "MANAGE_MESSAGES" permission in the channel.
-fn is_channel_mod(ctx: &mut Context, _: Option<GuildId>, ch: ChannelId, u: UserId) -> bool {
-    match ch.to_channel(&ctx) {
-        Ok(Channel::Guild(gc)) => {
-            let gc = gc.read();
-            gc.permissions_for_user(&ctx, u)
-                .map(|perms| perms.contains(Permissions::MANAGE_MESSAGES))
-                .unwrap_or(false)
-        }
+async fn is_channel_mod(ctx: &Context, _: Option<GuildId>, ch: ChannelId, u: UserId) -> bool {
+    match ch.to_channel(&ctx).await {
+        Ok(Channel::Guild(gc)) => gc
+            .permissions_for_user(&ctx, u)
+            .await
+            .map(|perms| perms.contains(Permissions::MANAGE_MESSAGES))
+            .unwrap_or(false),
         _ => false,
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    env_logger::init();
     // Setup dotenv
     if let Ok(path) = dotenv::dotenv() {
         println!("Loaded dotenv from {:?}", path);
     }
 
-    let mut handler = Handler::new();
+    let handler = Handler::new();
     // Set up hooks
     #[cfg(feature = "osu")]
     handler.hooks.push(youmubot_osu::discord::hook);
     #[cfg(feature = "codeforces")]
     handler.hooks.push(youmubot_cf::codeforces_info_hook);
 
+    // Collect the token
+    let token = var("TOKEN").expect("Please set TOKEN as the Discord Bot's token to be used.");
+    // Set up base framework
+    let fw = setup_framework(&token[..]).await;
+
     // Sets up a client
     let mut client = {
-        // Collect the token
-        let token = var("TOKEN").expect("Please set TOKEN as the Discord Bot's token to be used.");
         // Attempt to connect and set up a framework
-        Client::new(token, handler).expect("Cannot connect")
+        Client::new(token)
+            .framework(fw)
+            .event_handler(handler)
+            .intents(
+                GatewayIntents::GUILDS
+                    | GatewayIntents::GUILD_BANS
+                    | GatewayIntents::GUILD_MESSAGES
+                    | GatewayIntents::GUILD_MESSAGE_REACTIONS
+                    | GatewayIntents::DIRECT_MESSAGES
+                    | GatewayIntents::DIRECT_MESSAGE_REACTIONS,
+            )
+            .await
+            .unwrap()
     };
-
-    // Set up base framework
-    let mut fw = setup_framework(&client);
 
     // Set up announcer handler
     let mut announcers = AnnouncerHandler::new(&client);
 
     // Setup each package starting from the prelude.
     {
-        let mut data = client.data.write();
+        let mut data = client.data.write().await;
         let db_path = var("DBPATH")
             .map(|v| std::path::PathBuf::from(v))
             .unwrap_or_else(|e| {
                 println!("No DBPATH set up ({:?}), using `/data`", e);
                 std::path::PathBuf::from("data")
             });
-        youmubot_prelude::setup::setup_prelude(&db_path, &mut data, &mut fw);
+        youmubot_prelude::setup::setup_prelude(&db_path, &mut data);
         // Setup core
         #[cfg(feature = "core")]
         youmubot_core::setup(&db_path, &client, &mut data).expect("Setup db should succeed");
@@ -112,11 +115,10 @@ fn main() {
     #[cfg(feature = "codeforces")]
     println!("codeforces enabled.");
 
-    client.with_framework(fw);
-    announcers.scan(std::time::Duration::from_secs(120));
+    tokio::spawn(announcers.scan(std::time::Duration::from_secs(120)));
 
     println!("Starting...");
-    if let Err(v) = client.start() {
+    if let Err(v) = client.start().await {
         panic!(v)
     }
 
@@ -124,71 +126,43 @@ fn main() {
 }
 
 // Sets up a framework for a client
-fn setup_framework(client: &Client) -> StandardFramework {
+async fn setup_framework(token: &str) -> StandardFramework {
+    let http = serenity::http::Http::new_with_token(token);
     // Collect owners
-    let owner = client
-        .cache_and_http
-        .http
+    let owner = http
         .get_current_application_info()
+        .await
         .expect("Should be able to get app info")
         .owner;
 
-    let fw =    StandardFramework::new()
-            .configure(|c| {
-                c.with_whitespace(false)
-                    .prefix(&var("PREFIX").unwrap_or("y!".to_owned()))
-                    .delimiters(vec![" / ", "/ ", " /", "/"])
-                    .owners([owner.id].iter().cloned().collect())
-            })
-            .help(&youmubot_core::HELP)
-            .before(|_, msg, command_name| {
-                println!(
-                    "Got command '{}' by user '{}'",
-                    command_name, msg.author.name
-                );
-                true
-            })
-            .after(|ctx, msg, command_name, error| match error {
-                Ok(()) => println!("Processed command '{}'", command_name),
-                Err(why) => {
-                    let reply = format!("Command '{}' returned error {:?}", command_name, why);
-                    if let Err(_) = msg.reply(&ctx, &reply) {}
-                    println!("{}", reply)
-                }
-            })
-            .on_dispatch_error(|ctx, msg, error| {
-                msg.reply(
-                    &ctx,
-                    &match error {
-                        DispatchError::Ratelimited(seconds) => format!(
-                            "⏳ You are being rate-limited! Try this again in **{} seconds**.",
-                            seconds
-                        ),
-                        DispatchError::NotEnoughArguments { min, given } => format!("😕 The command needs at least **{}** arguments, I only got **{}**!\nDid you know command arguments are separated with a slash (`/`)?", min, given),
-                        DispatchError::TooManyArguments { max, given } => format!("😕 I can only handle at most **{}** arguments, but I got **{}**!", max, given),
-                        DispatchError::OnlyForGuilds => format!("🔇 This command cannot be used in DMs."),
-                        _ => return,
-                    },
-                )
-                .unwrap(); // Invoke
-            })
-            // Set a function that's called whenever an attempted command-call's
-            // command could not be found.
-            .unrecognised_command(|_, _, unknown_command_name| {
-                println!("Could not find command named '{}'", unknown_command_name);
-            })
-            // Set a function that's called whenever a message is not a command.
-            .normal_message(|_, _| {
-                // println!("Message is not a command '{}'", message.content);
-            })
-            .bucket("voting", |c| {
-                c.check(|ctx, g, ch, u| !is_channel_mod(ctx, g, ch, u)).delay(120 /* 2 minutes */).time_span(120).limit(1)
-            })
-            .bucket("images", |c| c.time_span(60).limit(2))
-            .bucket("community", |c| {
-                c.check(|ctx, g, ch, u| !is_channel_mod(ctx, g, ch, u)).delay(30).time_span(30).limit(1)
-            })
-            .group(&prelude_commands::PRELUDE_GROUP);
+    let fw = StandardFramework::new()
+        .configure(|c| {
+            c.with_whitespace(false)
+                .prefix(&var("PREFIX").unwrap_or("y!".to_owned()))
+                .delimiters(vec![" / ", "/ ", " /", "/"])
+                .owners([owner.id].iter().cloned().collect())
+        })
+        .help(&youmubot_core::HELP)
+        .before(before_hook)
+        .after(after_hook)
+        .on_dispatch_error(on_dispatch_error)
+        .bucket("voting", |c| {
+            c.check(|ctx, g, ch, u| Box::pin(async move { !is_channel_mod(ctx, g, ch, u).await }))
+                .delay(120 /* 2 minutes */)
+                .time_span(120)
+                .limit(1)
+        })
+        .await
+        .bucket("images", |c| c.time_span(60).limit(2))
+        .await
+        .bucket("community", |c| {
+            c.check(|ctx, g, ch, u| Box::pin(async move { !is_channel_mod(ctx, g, ch, u).await }))
+                .delay(30)
+                .time_span(30)
+                .limit(1)
+        })
+        .await
+        .group(&prelude_commands::PRELUDE_GROUP);
     // groups here
     #[cfg(feature = "core")]
     let fw = fw
@@ -200,4 +174,54 @@ fn setup_framework(client: &Client) -> StandardFramework {
     #[cfg(feature = "codeforces")]
     let fw = fw.group(&youmubot_cf::CODEFORCES_GROUP);
     fw
+}
+
+// Hooks!
+
+#[hook]
+async fn before_hook(_: &Context, msg: &Message, command_name: &str) -> bool {
+    println!(
+        "Got command '{}' by user '{}'",
+        command_name, msg.author.name
+    );
+    true
+}
+
+#[hook]
+async fn after_hook(ctx: &Context, msg: &Message, command_name: &str, error: CommandResult) {
+    match error {
+        Ok(()) => println!("Processed command '{}'", command_name),
+        Err(why) => {
+            let reply = format!("Command '{}' returned error {:?}", command_name, why);
+            msg.reply(&ctx, &reply).await.ok();
+            println!("{}", reply)
+        }
+    }
+}
+
+#[hook]
+async fn on_dispatch_error(ctx: &Context, msg: &Message, error: DispatchError) {
+    msg.reply(
+        &ctx,
+        &match error {
+            DispatchError::Ratelimited(seconds) => format!(
+                "⏳ You are being rate-limited! Try this again in **{} seconds**.",
+                seconds
+            ),
+            DispatchError::NotEnoughArguments { min, given } => {
+                format!(
+                    "😕 The command needs at least **{}** arguments, I only got **{}**!",
+                    min, given
+                ) + "\nDid you know command arguments are separated with a slash (`/`)?"
+            }
+            DispatchError::TooManyArguments { max, given } => format!(
+                "😕 I can only handle at most **{}** arguments, but I got **{}**!",
+                max, given
+            ),
+            DispatchError::OnlyForGuilds => format!("🔇 This command cannot be used in DMs."),
+            _ => return,
+        },
+    )
+    .await
+    .ok(); // Invoke
 }
