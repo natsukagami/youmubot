2449b09cb2ec728546103205ff14341ac89b3009
Natsu Kagami <natsukagami@gmail.com>
2021-11-03 15:00:18 -0400

    Create a Nix Flake module  (#17)
    * Add flake module
    
    * Add optional openssl dep on linux
    
    * Set default sql path
    
    * No more deploy!

--- flake.nix
+++ flake.nix
@@ -16,6 +16,11 @@
 
         root = ./.;
         cargoBuildOptions = opts: opts ++ [ "--package youmubot" ];
+
+        nativeBuildInputs = nixpkgs.lib.optionals (nixpkgs.lib.strings.hasSuffix "linux" system) (with pkgs; [
+          pkg-config
+          openssl
+        ]);
       };
 
       defaultPackage = packages.youmubot;
@@ -31,5 +36,8 @@
       devShell = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [ rustc cargo ];
       };
+
+      # module
+      nixosModule = import ./module.nix defaultPackage;
     });
 }
