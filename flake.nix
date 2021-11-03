6f844418238624e72583f25bedec457c2be92a90
Natsu Kagami <nki@nkagami.me>
2021-11-03 12:58:09 -0400

    Add flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A discord bot for Dự Tuyển Tổng Hợp server";
+  inputs = {
+    naersk.url = "github:nix-community/naersk";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, naersk, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages."${system}";
+      naersk-lib = naersk.lib."${system}";
+    in
+    rec {
+      packages.youmubot = naersk-lib.buildPackage {
+        name = "youmubot";
+        version = "0.1.0";
+
+        root = ./.;
+        cargoBuildOptions = opts: opts ++ [ "--package youmubot" ];
+      };
+
+      defaultPackage = packages.youmubot;
+
+      # `nix run`
+      apps.youmubot = flake-utils.lib.mkApp {
+        drv = packages.youmubot;
+        exePath = "/bin/youmubot";
+      };
+      defaultApp = apps.youmubot;
+
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [ rustc cargo ];
+      };
+    });
+}
