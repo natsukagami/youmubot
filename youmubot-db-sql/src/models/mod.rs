afea682408513068ede4cc1cc0985edb6076b4ef
Natsu Kagami <nki@nkagami.me>
2025-03-27 12:05:10 +0100

    New table `ignored_users` and queries

--- youmubot-db-sql/src/models/mod.rs
+++ youmubot-db-sql/src/models/mod.rs
@@ -4,5 +4,6 @@ use sqlx::{query, query_as, Executor};
 /// The DateTime used in the package.
 pub type DateTime = chrono::DateTime<chrono::Utc>;
 
+pub mod ignore_list;
 pub mod osu;
 pub mod osu_user;
